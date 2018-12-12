6c16beaa1d36835c41173aa106363a3ec780ffe0
AlexVotry <alexvotry@gmail.com>
2018-12-12 14:56:23 -0800

    send build to github

---
+++ client/build/precache-manifest.60f1dce456efb3bb349ce45c1386e71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2119618e3a10e15e94f3",
+    "url": "/static/js/main.2119618e.chunk.js"
+  },
+  {
+    "revision": "33780c95bb7d1f5ea60a",
+    "url": "/static/js/1.33780c95.chunk.js"
+  },
+  {
+    "revision": "33780c95bb7d1f5ea60a",
+    "url": "/static/css/1.422ccc06.chunk.css"
+  },
+  {
+    "revision": "192878e6e5204ec22dcc1cba66f2e636",
+    "url": "/index.html"
+  }
+];
