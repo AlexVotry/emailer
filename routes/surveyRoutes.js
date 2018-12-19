9bf8bca85ab46c5d6c5aef6ea1c5606e0ea018ae
AlexVotry <alexvotry@gmail.com>
2018-12-19 12:23:54 -0800

    save data to db, modify credits, error handle (try, catch)

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     // new syntax of title: title, is now just title,
@@ -18,9 +18,19 @@ module.exports = app => {
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
-    })
+    });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 }
