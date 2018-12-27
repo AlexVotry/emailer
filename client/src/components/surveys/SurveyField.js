6b99bffa825c210a06b877a319e13df5e9f3390f
AlexVotry <alexvotry@gmail.com>
2018-12-27 10:09:51 -0800

    show validate error on form (for title only)

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,12 +2,15 @@
 import React from 'react';
 
 // ({ input }) == (props.input)
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   // {...input} == all the properties on input object
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+        {touched && error}
     </div>
   )
 }
+
+// {touched && error} == if (touched && error) { error }
