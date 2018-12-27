f4b5b7e3b6aecf984f4cf0e7bd5bf3b4a1e5d5e9
AlexVotry <alexvotry@gmail.com>
2018-12-27 11:28:25 -0800

    added form review, added showReview state in SurveyNew

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,28 @@
 // SurveyNew shows SurveyForm and SurveyReview
-
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = { formReview: true };
+  // }
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
+
+    return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm/>
+        {this.renderContent()}
       </div>
     );
   }
