7e6e32d7ce5c78ce2bb2c42b1ec89c8ca7129749
AlexVotry <alexvotry@gmail.com>
2018-12-27 12:25:57 -0800

    finalize review fields, create separate file for fields array

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,25 @@
 // SurveyFormReview shows users their form inputs for review
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import formFields from './formFields';
 
-const SurveyFormReview = ({ onCancel }) => {
+
+const SurveyFormReview = ({ onCancel, formValues }) => {
+    const reviewFields = _.map(formFields, ({ label, name }) => {
+      return (
+        <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+        </div>
+      );
+    });
   return (
     <div>
       <h5>Please confirm your entries</h5>
-
+      <div>
+        {reviewFields}
+      </div>
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back<i className="material-icons left">arrow_back</i>
       </button>
