d5a62a222a8a029a1991e4aebf9be5f1a57b7b0b
AlexVotry <alexvotry@gmail.com>
2018-12-28 12:27:51 -0800

    redirect after posting survey

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,9 +16,11 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 }
 
-export const submitSurvey = values => async dispatch => {
+export const submitSurvey = (values, history) => async dispatch => {
+  console.log('history: ', history);
   const res = await axios.post('/api/surveys', values);
-
+  // following will navigate to /surveys after action is taken:
+  history.push('/surveys');
   dispatch( { type: FETCH_USER, payload: res.data });
 };
 
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ label, name }) => {
       return (
         <div key={name}>
@@ -25,7 +26,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
         Back<i className="material-icons left">arrow_back</i>
       </button>
       <button
-        onClick={() => submitSurvey(formValues)}
+        onClick={() => submitSurvey(formValues, history)}
         className="teal btn-flat right white-text">
         Send Survey<i className="material-icons right">done</i>
       </button>
@@ -38,4 +39,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
